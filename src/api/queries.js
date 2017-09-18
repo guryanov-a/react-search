8efb4696b6a6a609450042d0fc820fde9ebd84a5
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-09-18 20:25:17 +0600

    change ajax requests and server port

--- src/api/queries.js
+++ src/api/queries.js
@@ -58,7 +58,7 @@ export const searchQuery = () => {
 
   let resultQuery = `?${resultQueryArray.join('&')}`;
 
-  fetch(`//localhost:3004/articles${resultQuery}`).then( (response) => {
+  fetch(`:3004/articles${resultQuery}`).then( (response) => {
     const {
       totalSearchResults,
     } = store.getState();
@@ -83,7 +83,7 @@ export const searchQuery = () => {
 };
 
 export const filtersQuery = () => {
-  fetch(`//localhost:3004/articleTypes`)
+  fetch(`:3004/articleTypes`)
     .then(response => response.json())
     .then(articleTypes => {
       store.dispatch({
