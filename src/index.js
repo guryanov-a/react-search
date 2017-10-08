770b3ecd33b67c080bbd5e4ccd5186c19ce0aeac
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-10-08 11:19:59 +0600

    use Provider from react-redux package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchApp from './components/SearchApp';
-import { Provider } from "./utility/Provider";
+import { Provider } from 'react-redux';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
