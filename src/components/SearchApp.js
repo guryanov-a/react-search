bb0f4dd4e09d118b02425869815e89fd955307bd
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-10-26 18:27:54 +0600

    SearchApp: container -> presentational component

--- src/components/SearchApp.js
+++ src/components/SearchApp.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import SearchResults from './SearchResults';
 import ArticlesSearchForm from './ArticlesSearchForm';
 
-class SearchApp extends Component {
-  render() {
-    return (
-      <div className="search-app">
-        <div className="container-fluid">
-          <ArticlesSearchForm />
-          <SearchResults />
-        </div>
-      </div>
-    );
-  }
-}
+const SearchApp = () => (
+  <div className="search-app">
+    <div className="container-fluid">
+      <ArticlesSearchForm />
+      <SearchResults />
+    </div>
+  </div>
+);
 
 export default SearchApp;
