4dfdf315b8d9a17258abfead7bce8a4b6dc874fd
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-10-28 12:54:09 +0600

    ArticleTypeFilters: connect, container -> presentational

--- src/components/ArticleTypeFilters.js
+++ src/components/ArticleTypeFilters.js
@@ -1,40 +1,27 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
 import ArticleTypeTabs from './ArticleTypeTabs';
 import ArticleTypeSelect from './ArticleTypeSelect';
 
-class ArticleTypeFilters extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-
-    store.subscribe(() => {
-      this.forceUpdate();
-    });
+let ArticleTypeFilters = ({ areTabs }) => {
+  if(areTabs) {
+    return (
+      <div className="search-results__article-type-filters">
+        <ArticleTypeTabs />
+        <ArticleTypeSelect />
+      </div>
+    );
+  } else {
+    return null;
   }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const { store } = this.context;
-    const { areTabs } = store.getState();
-
-    if(areTabs) {
-      return (
-        <div className="search-results__article-type-filters">
-          <ArticleTypeTabs />
-          <ArticleTypeSelect />
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
-}
-
-ArticleTypeFilters.contextTypes = {
-  store: PropTypes.object,
 };
+
+const mapStateToProps = (state) => ({
+  areTabs: state.areTabs,
+});
+
+ArticleTypeFilters = connect(
+  mapStateToProps,
+)(ArticleTypeFilters);
 
 export default ArticleTypeFilters;
--- src/components/SearchApp.js
+++ src/components/SearchApp.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchResults from './SearchResults';
 import ArticlesSearchForm from './ArticlesSearchForm';
 
