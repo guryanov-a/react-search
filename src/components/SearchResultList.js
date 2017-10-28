9620de35c3c2488c6e401bb9d1e1a5b8b8ec79ba
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-10-28 13:16:46 +0600

    SearchResultList component: connect

--- src/components/SearchResultList.js
+++ src/components/SearchResultList.js
@@ -1,36 +1,13 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import PreviewsList from './PreviewsList';
 
-class SearchResultList extends Component {
-  componentDidMount() {
-    const { store } = this.context;
+const mapStateToProps = ({ searchResults, totalSearchResults }) => ({
+  previews: searchResults,
+  totalPreviews: totalSearchResults,
+});
 
-    store.subscribe(() => {
-      this.forceUpdate();
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const { store } = this.context;
-    const {
-      searchResults,
-      totalSearchResults,
-    } = store.getState();
-
-    return <PreviewsList
-      previews={searchResults}
-      totalPreviews={totalSearchResults}
-    />;
-  }
-}
-
-SearchResultList.contextTypes = {
-  store: PropTypes.object,
-};
+const SearchResultList = connect(
+  mapStateToProps,
+)(PreviewsList);
 
 export default SearchResultList;
