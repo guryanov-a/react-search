2da69a9c5b5561c46310f4419a7ccfcafd402996
Artyom Guryanov <guryanov.artyom@gmail.com>
2017-10-31 22:36:22 +0600

    Wrapping dispatch() to Log Actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,27 @@
 import { createStore } from 'redux';
 import { searchApp } from '../reducers';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+
+  if(!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+
+    const returnValue = rawDispatch(action);
+
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = Object.assign(
     {},
@@ -24,11 +45,17 @@ const configureStore = () => {
     },
   );
 
-  return createStore(
+  const store = createStore(
     searchApp,
     persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
+
+  if(process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
+  return store;
 };
 
 export default configureStore;
